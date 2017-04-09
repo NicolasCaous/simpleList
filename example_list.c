3375b0f8a3aafcf196c3f1255859febe4caaef52
Nicolas Caous Fernandes <caousn@hotmail.com>
2017-04-08 23:45:58 -0300

    Initial Commit
    Base Implementation of features

---
+++ example_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "./lib/utils.h"
+
+typedef struct value {
+	char * s;
+} value;
+
+
+int main(int argc, char** argv) {
+	list l = createList();
+	value tmp = {"beta"};
+	insertEndList(l, &tmp, sizeof(tmp));
+	tmp.s = "delta";
+	insertEndList(l, &tmp, sizeof(tmp));
+	tmp.s = "alpha";
+	insertBegList(l, &tmp, sizeof(tmp));
+	tmp.s = "gamma";
+	insertAtList(l, &tmp, sizeof(tmp), 2);
+	tmp.s = "junk";
+	insertBegList(l, &tmp, sizeof(tmp));
+	removeBegList(l);
+	insertEndList(l, &tmp, sizeof(tmp));
+	removeEndList(l);
+	insertAtList(l, &tmp, sizeof(tmp), 2);
+	removeAtList(l, 2);
+	int i;
+	for(i=0; i<listSize(l); ++i) {
+		printf("%s\n", listGetAt(l, i)->s);
+	}
+	printf("BEG: %s\n", listGetFirst(l)->s);
+	printf("END: %s\n", listGetLast(l)->s);
+	return 0;
+}
